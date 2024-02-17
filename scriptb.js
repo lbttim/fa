a80247f29bd94816ffad6157b73f8ace8d69fca4
ttimlb <tim.lajevec@gmail.com>
2024-02-17 21:33:13 +0100

    Update and rename scriptq.js to scriptb.js

--- scriptb.js
+++ scriptb.js
@@ -9,11 +9,17 @@
 	  
 	  	
 
-    var files = eval('<?php echo "aaaa";?>');
-  alert(files);
     var button = document.createElement('button');
     button.textContent = 'Click Me';
     button.addEventListener('click', function() {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', 'https://cdn.jsdelivr.net/gh/ttimhzv/fa@main/main.php', true);
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    document.getElementById('output').innerHTML = xhr.responseText;
+                }
+            };
+            xhr.send();
       alert('Button clicked!');
     });
     div.appendChild(button);
